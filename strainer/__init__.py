66e09f7ca6a8a21b2cc62f75f2a3ca8d10ea21dd
Alex Kessinger <akessinger@bepress.com>
2017-05-24 20:57:12 -0600

    Bumping to 1.2.0

--- strainer/__init__.py
+++ strainer/__init__.py
@@ -1,13 +1,15 @@
 __title__ = 'strainer'
-__version__ = '1.1.1'
+__version__ = '1.2.0'
 __author__ = 'Alex Kessinger'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Alex Kessiger'
 
 from .structure import (serializer, many, child, field, dict_field, multiple_field)
 from .exceptions import ValidationException
+from .context import SerializationContext, check_context
 from strainer import formatters
 from strainer import validators
 
 __all__ = (serializer, many, child, field, dict_field,
-           ValidationException, formatters, validators, multiple_field)
+           ValidationException, formatters, validators, multiple_field,
+           SerializationContext, check_context)
